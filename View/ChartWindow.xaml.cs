4f26e0cbc9d108497c0a23f3be7e4763045d8b1c
Norbert <norudzki@gmail.com>
2021-03-23 03:10:36 +0100

    Added signal loading from files

--- View/ChartWindow.xaml.cs
+++ View/ChartWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using LiveCharts;
 using LiveCharts.Defaults;
@@ -10,10 +12,17 @@ namespace SignalProcessing.View
     public partial class ChartWindow : Window
     {
 
-        public ChartWindow(Signal signal)
+        public ChartWindow(Signal signal, int sectionAmount = 15)
         {
             InitializeComponent();
-            
+            var sb = new StringBuilder();
+            sb.Append("Average = ").Append(Math.Round( signal.Average(),6));
+            sb.Append("\nAbsoluteAverage = ").Append(Math.Round(signal.AbsoluteAverage(),6));
+            sb.Append("\nRootMeanSquare = ").Append(Math.Round(signal.RootMeanSquare(),6));
+            sb.Append("\nVariation = ").Append(Math.Round(signal.Variation(),6));
+            sb.Append("\nAveragePower = ").Append(Math.Round(signal.AveragePower(),6));
+            sb.Append("\n================\n").Append(signal.ToString());
+            SignalText.Text = sb.ToString();
             SignalChart.AxisY.Clear();
             SignalChart.AxisY.Add(
                 new Axis
@@ -56,7 +65,7 @@ namespace SignalProcessing.View
             {
                 new ColumnSeries
                 {
-                    Values = new ChartValues<ObservablePoint>(signal.GetHistogramPlot(15)),
+                    Values = new ChartValues<ObservablePoint>(signal.GetHistogramPlot(sectionAmount)),
                     PointGeometry = null,
                 }
             };
--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -29,9 +29,9 @@ namespace SignalProcessing
     /// </summary>
     public partial class MainWindow : Window
     {
-        public CartesianChart SignalChart;
-        public CartesianChart Histogram;
-
+        private Signal _loadedSignal1;
+        private Signal _loadedSignal2;
+        
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
@@ -44,13 +44,8 @@ namespace SignalProcessing
             SignalComboBox.ItemsSource = Enum.GetValues(typeof(SignalGenerator.Type)).Cast<SignalGenerator.Type>();
         }
 
-        public void ShowChart(object obj, RoutedEventArgs routedEventArgs)
+        public void GenerateChart(object obj, RoutedEventArgs routedEventArgs)
         {
-
-            
-          
-            SignalChart = (CartesianChart)FindName("SignalChart");
-            Histogram = (CartesianChart)FindName("Histogram");
 
             SignalGenerator signalGenerator = new SignalGenerator
             {
@@ -58,25 +53,122 @@ namespace SignalProcessing
                 Frequency = 100,
                 Duration = 10
             };
-            signalGenerator.JumpTime = 5;
-            
+            double temporaryInputValue;
+            if (double.TryParse(InputAmplitude.Text, out temporaryInputValue))
+            {
+                signalGenerator.Amplitude = double.Parse(InputAmplitude.Text);
+            }
+            if (double.TryParse(InputStartTime.Text, out temporaryInputValue))
+            {
+                signalGenerator.StartTime = double.Parse(InputStartTime.Text);
+            }
+            if (double.TryParse(InputDuration.Text, out temporaryInputValue))
+            {
+                signalGenerator.Duration = double.Parse(InputDuration.Text);
+            }
+            if (double.TryParse(InputPeriod.Text, out temporaryInputValue))
+            {
+                signalGenerator.Period = double.Parse(InputPeriod.Text);
+            }
+            if (double.TryParse(InputFillFactor.Text, out temporaryInputValue))
+            {
+                signalGenerator.FillFactor = double.Parse(InputFillFactor.Text);
+            }
+            if (double.TryParse(InputJumpTime.Text, out temporaryInputValue))
+            {
+                signalGenerator.JumpTime = double.Parse(InputJumpTime.Text);
+            }
+            if (double.TryParse(InputProbability.Text, out temporaryInputValue))
+            {
+                signalGenerator.Probability = double.Parse(InputProbability.Text);
+            }
+            if (double.TryParse(InputFrequency.Text, out temporaryInputValue))
+            {
+                signalGenerator.Frequency = double.Parse(InputFrequency.Text);
+            }
+
+            int sectionAmount = 15;
+            sectionAmount = int.TryParse(HistogramInterval.Text, out sectionAmount) ? int.Parse(HistogramInterval.Text) : 15;
             Signal signal = signalGenerator.Generate((SignalGenerator.Type)SignalComboBox.SelectionBoxItem);
 
-            Stream stream = File.Open("Signal.sig", FileMode.Create);
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-
-            binaryFormatter.Serialize(stream, signal);
-
-            stream.Close();
-
-            signal = null;
-
-            stream = File.Open("Signal.sig", FileMode.Open);
-
-            signal = (Signal)binaryFormatter.Deserialize(stream);
-
-            Window chartWindow = new ChartWindow(signal);
+            SaveSignalToFile(signal,"generated");
+           
+            Window chartWindow = new ChartWindow(signal, sectionAmount);
             chartWindow.Show();
         }
+
+        public void SaveSignalToFile(Signal signal, String prefix)
+        {
+            Stream stream = File.Open("signals/"+prefix+"_signal_@"+DateTime.Now.ToString("T").Replace(':','-')+".sig", FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, signal);
+            stream.Close();
+        }
+        public void LoadSignal1(object obj, RoutedEventArgs routedEventArgs)
+        {
+            var (signalFromFile, fileName) = LoadSignalFromFile();
+            if (signalFromFile == null) return;
+            _loadedSignal1 = signalFromFile;
+            SignalName1.Text = fileName;
+        }
+        
+        public void LoadSignal2(object obj, RoutedEventArgs routedEventArgs)
+        {
+            var (signalFromFile, fileName) = LoadSignalFromFile();
+            if (signalFromFile == null) return;
+            _loadedSignal2 = signalFromFile;
+            SignalName2.Text = fileName;
+        }
+
+        public (Signal, String) LoadSignalFromFile()
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog {DefaultExt = ".sig"};
+            var result = dlg.ShowDialog();
+            if (result != true) return (null, "");
+            var filename = dlg.FileName;
+            var binaryFormatter = new BinaryFormatter();
+            Stream stream = File.Open(filename, FileMode.Open);
+            var signal = (Signal)binaryFormatter.Deserialize(stream);
+            Window chartWindow = new ChartWindow(signal, int.TryParse(HistogramInterval.Text, out _) ? int.Parse(HistogramInterval.Text) : 15);
+            chartWindow.Show();
+            return (signal, dlg.SafeFileName);
+        }
+        
+        public void AddSignals(object obj, RoutedEventArgs routedEventArgs)
+        {
+            if (_loadedSignal1 == null && _loadedSignal2 == null) return;
+            Signal sum = SignalOperations.Add(_loadedSignal1, _loadedSignal2);
+            Window chartWindow = new ChartWindow(sum, int.TryParse(HistogramInterval.Text, out _) ? int.Parse(HistogramInterval.Text) : 15);
+            chartWindow.Show();
+            SaveSignalToFile(sum,"ADD");
+        }
+        
+        public void SubtractSignals(object obj, RoutedEventArgs routedEventArgs)
+        {
+            if (_loadedSignal1 == null && _loadedSignal2 == null) return;
+            Signal difference = SignalOperations.Subtract(_loadedSignal1, _loadedSignal2);
+            Window chartWindow = new ChartWindow(difference, int.TryParse(HistogramInterval.Text, out _) ? int.Parse(HistogramInterval.Text) : 15);
+            chartWindow.Show();
+            SaveSignalToFile(difference,"SUBTRACT");
+        }
+        
+        public void MultiplySignals(object obj, RoutedEventArgs routedEventArgs)
+        {
+            if (_loadedSignal1 == null && _loadedSignal2 == null) return;
+            Signal product = SignalOperations.Multiply(_loadedSignal1, _loadedSignal2);
+            Window chartWindow = new ChartWindow(product, int.TryParse(HistogramInterval.Text, out _) ? int.Parse(HistogramInterval.Text) : 15);
+            chartWindow.Show();
+            SaveSignalToFile(product,"MULTIPLY");
+        }
+        
+        public void DivideSignals(object obj, RoutedEventArgs routedEventArgs)
+        {
+            if (_loadedSignal1 == null && _loadedSignal2 == null) return;
+            Signal quotient = SignalOperations.Divide(_loadedSignal1, _loadedSignal2);
+            Window chartWindow = new ChartWindow(quotient, int.TryParse(HistogramInterval.Text, out _) ? int.Parse(HistogramInterval.Text) : 15);
+            chartWindow.Show();
+            SaveSignalToFile(quotient,"DIVIDE");
+        }
+        
     }
-}
+}    
